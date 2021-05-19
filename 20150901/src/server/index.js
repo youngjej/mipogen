2487959b4b83b6fd08a90f69402ff3c90e03fdf6
Youngje Jo <youngjej@kaist.ac.kr>
2021-05-20 05:18:20 +0900

    pages

--- 20150901/src/server/index.js
+++ 20150901/src/server/index.js
@@ -1,24 +1,40 @@
-const path = require("path");
-const express = require("express");
+// const path = require("path");
+// const express = require("express");
 
-const port = process.env.PORT || 3000;
+// const port = process.env.PORT || 3000;
 
-const app = express();
-const server = require('http').createServer(app);
-
-server.listen(port, function() {
-    console.log("Server listening at port %d", port);
-});
-
-app.use(express.static(__dirname + '../client/'));
-
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/index.html'));
-});
-
-// const express = require('express');
 // const app = express();
+// const server = require('http').createServer(app);
 
-// const server = app.listen(3000, () => {
-//   console.log('Server connected.');
-// })
+// server.listen(port, function() {
+//     console.log("Server listening at port %d", port);
+// });
+
+// app.use(express.static(__dirname + '../client/'));
+
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, '../client/index.html'));
+// });
+
+const express = require('express');
+const app = express();
+
+const server = app.listen(3000, () => {
+  console.log('Server connected.');
+})
+
+app.set('views', __dirname + "/../client/");
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+app.get('/', function(req, res) {
+  res.render('index.html');
+});
+
+app.get('/login', function(req, res) {
+  res.render('login.html');
+});
+
+app.get('/generate', function(req, res) {
+  res.render('generate.html');
+});
